51b4742443e6c8a0433d73ec24eb92f295d0bcac
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-02-09 20:00:56 -0300

    Update prompt message for user input
    teste return from git

--- src/main/java/org/example/ReadBytes.java
+++ src/main/java/org/example/ReadBytes.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class ReadBytes {
     public static void main(String[] args) throws IOException {
         byte data[] = new byte[10];
-        System.out.println("Enter some characcccccccccccccccccc: ");
+        System.out.println("Enter some data: ");
         System.in.read(data);
         System.out.print("You entered: ");
         for(int i = 0; i < data.length; i++){
