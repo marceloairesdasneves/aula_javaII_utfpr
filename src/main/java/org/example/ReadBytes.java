90d1f58e4f2b84d07334f4f432138df92b8febf8
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-02-09 20:14:07 -0300

    Update prompt message in ReadBytes class
    Enter some data(Testing new

--- src/main/java/org/example/ReadBytes.java
+++ src/main/java/org/example/ReadBytes.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class ReadBytes {
     public static void main(String[] args) throws IOException {
         byte data[] = new byte[10];
-        System.out.println("Enter some data(Testing): ");
+        System.out.println("Enter some data(Testing new): ");
         System.in.read(data);
         System.out.print("You entered: ");
         for(int i = 0; i < data.length; i++){
