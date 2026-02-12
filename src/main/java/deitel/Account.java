d48dee0dd71833a59fc70d67a00e2a903d33b785
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-02-12 08:52:31 -0300

    Testing exe Deitel Account example.

---
+++ src/main/java/deitel/Account.java
@@ -0,0 +1,16 @@
+package deitel;
+
+public class Account {
+    private String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
